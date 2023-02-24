ad081d3aa27e434e1d6a494e0614bbaafde50230
olimarteam <73653954+olimarteam@users.noreply.github.com>
2023-02-23 23:47:38 -0300

    semana 2 y pruebas

---
+++ Front end 2/playground C4A/js/script.js
@@ -0,0 +1,15 @@
+
+let elH1 = document.querySelector('h1');
+let elUl = document.querySelector('.nav-bar');
+let allLi = document.querySelectorAll('li');
+let elLi = document.querySelector('li');
+
+console.log(elH1);
+console.log(elUl);
+console.log(allLi);
+console.log(elLi);
+
+console.log('hola nene2')
+console.log('hola 11424245')
+console.log('hola 1141245455')
+console.log('hola 455das5dsadsadsadsadsadsadsadsadsa')
